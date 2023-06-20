dfff51a6fd514133c41b956d0a8753b89b235211
Matheus Pereira <matpereira@microsoft.com>
2023-06-20 11:57:30 -0400

    added gpt-4-32k and gpt-35-turbo

--- dln/operator.py
+++ dln/operator.py
@@ -25,6 +25,7 @@ class GPT:
         "text-babbage-001",
         "text-ada-001",
         "gpt-4",
+        "gpt-4-32k",
     ]
 
     def __init__(self, model_name="text-davinci-003", **generation_options):
@@ -213,7 +214,7 @@ class GPT:
         generation_options = self.generation_options.copy()
         generation_options.update(**kwargs)
 
-        if self.engine in ("gpt-4", "gpt-4-32k"):
+        if self.engine in ("gpt-35-turbo", "gpt-4", "gpt-4-32k"):
             if "return_logprobs" in generation_options:
                 del generation_options["return_logprobs"]
 
