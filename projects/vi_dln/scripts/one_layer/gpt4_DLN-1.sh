0d1403efaff5a90f7d1c8f7d22218279df8d5f3c
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2023-08-10 17:28:18 +0000

    scritps

--- projects/vi_dln/scripts/one_layer/gpt4_DLN-1.sh
+++ projects/vi_dln/scripts/one_layer/gpt4_DLN-1.sh
@@ -1,24 +1,22 @@
 set -x  # print commands to terminal
 p_class_tpl="classify_forward:3.0"
+q_prompt_tpl="q_action_prompt:v3.5"
 iters=20
-batch_size=10
-num_p_samples=15
+batch_size=20
+num_p_samples=20
 bwd_temp=0.7
 held_out_prompt_ranking=True
 use_memory=2
 tolerance=2
 num_h_samples=5
-q_prompt_tpl="q_action_prompt:v3.5"
 logp_penalty=2.
 posterior_temp=1.
 model_type="gpt-4"
 
 # dataset
-for dataset in date_understanding logical_deduction_seven_objects subj navigate; do
-# n_shot
-for n_shot in 0 5 16; do
+for dataset in subj; do
 
-dir=log/gpt4/cost/${dataset}/${n_shot}shot/
+dir=log/gpt4/${dataset}/dln-1-20bsz/
 
 if [ ! -f ${dir}/done.txt ]; then
     /bin/rm -rf ${dir}
@@ -26,10 +24,11 @@ if [ ! -f ${dir}/done.txt ]; then
     # seed
     for seed in 13 42 25; do
         python vi_main.py \
-            --compute_cost \
+            --do_first_eval \
             --balance_batch \
-            --do_few_shot ${n_shot} \
             --one_layer \
+            --train_p1 False \
+            --train_p2 True \
             --num_p_samples ${num_p_samples} \
             --bwd_temp ${bwd_temp} \
             --iters ${iters} \
@@ -49,7 +48,5 @@ if [ ! -f ${dir}/done.txt ]; then
 
     touch ${dir}/done.txt
 fi
-# n shot
-done
 # dataset
 done
