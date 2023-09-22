98547667a07d99146f4fb27355ccd6b79f8d2a90
Matheus Pereira <matpereira@microsoft.com>
2023-09-22 13:58:04 -0400

    Display text in a table. Fix legends. Fix one layer prompt candidates

--- projects/demo/demo_streamlit.py
+++ projects/demo/demo_streamlit.py
@@ -9,9 +9,6 @@ import numpy as np
 import pandas as pd
 from jinja2 import Template
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
 
 forward_template_L1 = Template(
     "{{ input }}\n\n{{ prompt }} Let's think step by step."
@@ -77,149 +74,11 @@ def load_data(log_file, dataset):
         pd.DataFrame(flattened_examples),
     )
 
+
 def load_dataset_names(log_file):
     with open(log_file) as f:
         logs = json.load(f)
     return [(x, x) for x in list(logs.keys())]
-    example_dropdown = 0
-    df, candidates, examples = load_data(
-        args.logfile or "data.json", dataset_id
-    )
-
-    # Merge layers and examples
-    df = df.merge(examples, on="step", how="left")
-
-    EXAMPLE_ID = example_dropdown or 1
-    dev_df = df
-    dev_df = df[df["id"] == EXAMPLE_ID]
-    dev_df = dev_df[dev_df["dev_acc"] >= 0]
-
-    NB_LAYERS = len([c for c in dev_df.columns if c.startswith("layer")])
-
-    if example_dropdown == 0:
-        if NB_LAYERS == 1:
-            layers_columns = [c for c in dev_df.columns if c.startswith("layer")]
-            for column in layers_columns:
-                # Wrap text for display in hover
-                dev_df[column] = dev_df[column].apply(
-                    lambda x: x.replace("\n", "<br>")
-                )
-
-            hover_template = "<b> prompt:</b> %{customdata[0]}"
-
-        elif NB_LAYERS == 2:
-            layers_columns = [c for c in dev_df.columns if c.startswith("layer")]
-            for column in layers_columns:
-                # Wrap text for display in hover
-                dev_df[column] = dev_df[column].apply(
-                    lambda x: x.replace("\n", "<br>")
-                )
-
-            hover_template = (
-                "<b>Layer 1 prompt:</b> %{customdata[0]}"
-                "<br><b>Layer 2 prompt:</b> %{customdata[1]}"
-            )
-    else:
-        if NB_LAYERS == 1:
-            layers_columns = [
-                c for c in dev_df.columns if c.startswith("layer")
-            ] + ["input", "output", "label"]
-            for column in layers_columns:
-                # Wrap text for display in hover
-                dev_df[column] = dev_df[column].apply(
-                    lambda x: x.replace("\n", "<br>")
-                )
-
-            hover_template = (
-                "<b>Input:</b> %{customdata[1]}"
-                "<br><b>Layer 1 prompt:</b> %{customdata[0]}"
-                "<br><b>Output:</b> %{customdata[2]}"
-                "<br><b>Label:</b> %{customdata[3]}"
-            )
-
-        elif NB_LAYERS == 2:
-            layers_columns = [
-                c for c in dev_df.columns if c.startswith("layer")
-            ] + ["input", "hidden", "output", "label"]
-            for column in layers_columns:
-                # Wrap text for display in hover
-                dev_df[column] = dev_df[column].apply(
-                    lambda x: x.replace("\n", "<br>")
-                )
-
-            hover_template = (
-                "<b>Input:</b> %{customdata[2]}"
-                "<br><b>Layer 1 prompt:</b> %{customdata[0]}"
-                "<br><b>Hidden:</b> %{customdata[3]}"
-                "<br><b>Layer 2 prompt:</b> %{customdata[1]}"
-                "<br><b>Output:</b> %{customdata[4]}"
-                "<br><b>Label:</b> %{customdata[5]}"
-            )
-        else:
-            raise NotImplementedError()
-
-    hover_config = {
-        "customdata": dev_df[layers_columns],
-        "hovertemplate": hover_template,
-    }
-
-    # Create figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-    # Make Figure taller
-    fig.update_layout(
-        autosize=False,
-        width=1900,
-        height=1000,
-        margin=dict(l=50, r=50, b=100, t=100, pad=4),
-    )
-
-    # Add traces
-    # text = ["Acc"] * len(df["step"])
-    fig.add_trace(
-        go.Scatter(
-            x=df["step"], y=df["run_acc"], name="Running acc", hoverinfo="none"
-        ),  # , text=text, **hover_config
-        secondary_y=False,
-    )
-
-    # text = ["Dev Acc"] * len(df["step"][df["dev_acc"] >= 0])
-    fig.add_trace(
-        go.Scatter(
-            x=dev_df["step"], y=dev_df["dev_acc"], name="Dev acc", **hover_config
-        ),
-        secondary_y=False,
-    )
-
-    # text = ["ELBO"] * len(df["step"])
-    fig.add_trace(
-        go.Scatter(
-            x=df["step"],
-            y=df["run_elbo"],
-            name="Running ELBO",
-            hoverinfo="none",
-            visible="legendonly",
-        ),  # , text=text, **hover_config),
-        secondary_y=True,
-    )
-
-    # Set x-axes title
-    fig.update_xaxes(title_text="steps", nticks=20)
-
-    # Set y-axes titles
-    fig.update_yaxes(title_text="<b>Accuracy</b>", secondary_y=False)
-    fig.update_yaxes(title_text="<b>ELBO</b>", secondary_y=True)
-
-    # Define hover text font and color.
-    fig.update_layout(hovermode="x")
-    fig.update_layout(
-        hoverlabel=dict(
-            bgcolor="rgba(255,255,255,0.75)",
-            font_size=16,
-            font_family="Rockwell",
-        ),
-    )
-    return fig
 
 
 def main(args):
@@ -239,23 +98,31 @@ def main(args):
         highlight_step = st.slider("Step", 1, 20)
 
         st.write("")
-
-        st.markdown(f"**Input:** {examples[examples['step'] == highlight_step]['input'].iloc[highlight_example]}")
-        st.markdown(f"**Layer 1 prompt:** {df[df['step'] == highlight_step]['layer_1'].values[0]}")
+        table_data = []
+        table_data.append(f"| **Input:** | {examples[examples['step'] == highlight_step]['input'].iloc[highlight_example]}")
+        table_data.append(f"| **Layer 1 prompt:** | {df[df['step'] == highlight_step]['layer_1'].values[0]}")
         if 'layer_2' in df.columns:
-            st.markdown(f"**Hidden:** {examples[examples['step'] == highlight_step]['hidden'].iloc[highlight_example]}")
-            st.markdown(f"**Layer 2 prompt:** {df[df['step'] == highlight_step]['layer_2'].values[0]}")
-        st.markdown(f"**Output:** {examples[examples['step'] == highlight_step]['output'].iloc[highlight_example]}")
-        st.markdown(f"**Label:** {examples[examples['step'] == highlight_step]['label'].iloc[highlight_example]}")
+            table_data.append(f"| **Hidden:** | {examples[examples['step'] == highlight_step]['hidden'].iloc[highlight_example]}")
+            table_data.append(f"| **Layer 2 prompt:** | {df[df['step'] == highlight_step]['layer_2'].values[0]}")
+        table_data.append(f"| **Output:** | {examples[examples['step'] == highlight_step]['output'].iloc[highlight_example]}")
+        table_data.append(f"| **Label:** | {examples[examples['step'] == highlight_step]['label'].iloc[highlight_example]}")
+        table_data = [x.replace('\n', '<br>') for x in table_data]
+        table_data_str = "\n".join(table_data)
+
+        st.markdown(f"\n| | |\n| --- | --- |\n{table_data_str}", unsafe_allow_html=True)
+        st.write("")
 
     with col2:
-        melted_df = df.melt(id_vars=['step'], value_vars=['acc', 'run_acc'], var_name='metric', value_name='value')  
-  
-        # Create a line chart  
-        combined_chart = alt.Chart(melted_df).mark_line().encode(  
-            y=alt.Y('value:Q', scale=alt.Scale(domain=[0.4, 1.0])),  
-            x='step:Q',  
-            color=alt.Color('metric:N', scale=alt.Scale(domain=['acc', 'run_acc'], range=['steelblue', 'lightblue']), legend=alt.Legend(title="Accuracy")),  
+        melted_df = df.melt(id_vars=['step'], value_vars=['acc', 'run_acc'], var_name='metric', value_name='value')
+        melted_df['metric'] = melted_df['metric'].replace(['acc', 'run_acc'], ['Batch', 'Run Avg'])
+        combined_chart = alt.Chart(melted_df).mark_line().encode(
+            y=alt.Y('value:Q', title="accuracy", scale=alt.Scale(domain=[0.4, 1.0])),
+            x='step:Q',
+            color=alt.Color(
+                'metric:N',
+                scale=alt.Scale(domain=['Batch', 'Run Avg'], range=['steelblue', 'lightblue']),
+                legend=alt.Legend(title="Train Accuracy")
+            ),
         )
 
         # # Highlight a specific step  
@@ -281,10 +148,10 @@ def main(args):
 
     prompt_candidates = st.toggle("Prompt Candidates")
     if prompt_candidates:
+        # list all columns from candidates dataframe except the 'step' columns
+        cols = [col for col in candidates.columns if col != 'step']
         st.dataframe(
-            candidates[candidates["step"] == highlight_step][
-                ["layer_1_candidate", "layer_1_score", "layer_2_candidate", "layer_2_score"]
-            ],
+            candidates[candidates["step"] == highlight_step][cols],
             hide_index=True,
             use_container_width=True,
         )
