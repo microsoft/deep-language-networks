e2b51eb5672f4598b4c39d04bcea760d79942d3b
Matheus Pereira <matpereira@microsoft.com>
2023-10-10 12:58:02 -0700

    Fix logp test since DLNTemplate strips rendered template

--- tests/test_vi_layers.py
+++ tests/test_vi_layers.py
@@ -70,7 +70,7 @@ def test_log_p_without_output_classes(raw_logprobs, score_requests):
     prior_layer = PriorLayer(forward_template="suffix_forward", init="")
     with patch("dln.score.forward_evaluate", raw_logprobs):
         logp = prior_layer.log_p(inputs, outputs)
-    np.testing.assert_almost_equal(logp.logp_targets, [-1.48348267, -1.47351816])
+    np.testing.assert_almost_equal(logp.logp_targets, [-0.7682657, -0.7632834])
 
 
 def test_forward_with_output_class(top_logprobs):
