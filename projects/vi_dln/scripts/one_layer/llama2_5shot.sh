c4cf3448fd7937191febfdef8efea8b5c3535048
Matheus Pereira <matpereira@microsoft.com>
2023-10-24 09:22:14 -0700

    5 shot

--- projects/vi_dln/scripts/one_layer/llama2_5shot.sh
+++ projects/vi_dln/scripts/one_layer/llama2_5shot.sh
@@ -1,5 +1,5 @@
 set -x  # print commands to terminal
-datasets_list="navigate hyperbaton date_understanding logical_deduction_seven_objects mpqa trec subj disaster airline"
+datasets_list="subj navigate date_understanding logical_deduction_seven_objects hyperbaton mpqa trec disaster airline"
 p_class_tpl="classify_forward:3.0"
 num_p_samples=10
 held_out_prompt_ranking=True
@@ -7,12 +7,13 @@ fwd_max_tokens=512
 bwd_max_tokens=1024
 p2_max_tokens=512
 
+n_shot=5
 connections_config=scripts/one_layer/connections.yaml
 model_type="meta-llama/Llama-2-70b-chat-hf"
 
 for dataset in $datasets_list; do
 
-    dir=log/${model_type}/one_layer/zs/${dataset}
+    dir=log/${model_type}/one_layer/5_shot/${dataset}
 
     for seed in 13 42 25; do
         python vi_main.py \
@@ -23,6 +24,7 @@ for dataset in $datasets_list; do
             --balance_batch \
             --seed ${seed} \
             --dataset ${dataset} \
+            --n_shots ${n_shot} \
             --model_type ${model_type} \
             --num_p_samples ${num_p_samples} \
             --p_class ${p_class_tpl} \
