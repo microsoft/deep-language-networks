096fddc2ec3ddd35dc14eead70f6857633708be4
Matheus Pereira <matpereira@microsoft.com>
2023-11-06 08:42:35 -0800

    Added NumberPresenceLoss. Refac loss.

--- dln/loss.py
+++ dln/loss.py
@@ -1,36 +1,94 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+import re
+from typing import Callable, Iterable, Optional, Union
 
 import numpy as np
 
 
 class LLoss(ABC):
-    pass
 
+    @classmethod
+    def instantiate(cls, loss_type: str, postproc: Optional[Callable] = None) -> "LLoss":
+        losses = {
+            "ZeroOneLoss": ZeroOneLoss,
+            "NumberPresenceLoss": NumberPresenceLoss,
+        }
+        try:
+            return losses[loss_type](postproc)
+        except KeyError:
+            raise ValueError(f'Unknown loss type: {loss_type}')
 
-class ZeroOneLoss(LLoss):
-    def __init__(self, postproc=None):
+    def __init__(self, postproc: Optional[Callable] = None):
         """
         Args:
             postproc: a function that takes and returns a string to be apply before calculating the loss
-        Returns:
-            ZeroOneLoss as an float32 np.array
         """
         self._postproc = postproc
 
-    def __call__(self, input, target):
-        """
-        Args:
-            input: a list of strings
-            target: a list of strings
-        """
-        if self._postproc:
-            input = [self.postproc(i) for i in input]
-            target = [self.postproc(t) for t in target]
-        losses = (np.array(input) != np.array(target)).astype("float32")
-        return losses
-
     @property
     def postproc(self):
+        """
+        Returns the post-processing function for the loss function.
+        If the post-processing function has not been set, returns the identity function
+        """
         if self._postproc is None:
             return lambda x: x
         return self._postproc
+
+    @abstractmethod
+    def loss(self, inputs: Iterable[str], targets: Iterable[str]) -> np.array:
+        """
+        Computes the loss between the input and target
+        Args:
+            input: The predicted outputs
+            target: The true outputs
+        Returns:
+            The computed loss
+        """
+        pass
+
+    def __call__(
+        self,
+        inputs: Union[str, Iterable[str]],
+        targets: Union[str, Iterable[str]],
+    ) -> np.array:
+        """
+        Calls the loss function. If inputs or targets are not iterables, they are converted to lists
+        Args:
+            inputs: The predicted outputs
+            targets: The true outputs
+        Returns:
+            The computed loss as an np.array
+        """
+        if isinstance(inputs, str) or not isinstance(inputs, Iterable):
+            inputs = [inputs]
+        if isinstance(targets, str) or not isinstance(targets, Iterable):
+            targets = [targets]
+        if self._postproc:
+            inputs = [self.postproc(i) for i in inputs]
+            targets = [self.postproc(t) for t in targets]
+        losses = self.loss(inputs, targets)
+        return losses
+
+
+class ZeroOneLoss(LLoss):
+    """
+    Calculates the zero-one loss between the predicted and target outputs,
+    where 0 indicates a correct prediction and 1 indicates an incorrect prediction.
+    """
+    def loss(self, inputs: Iterable[str], targets: Iterable[str]) -> np.array:
+        losses = (np.array(inputs) != np.array(targets)).astype("float32")
+        return losses
+
+
+class NumberPresenceLoss(LLoss):
+    """
+    Calculates the loss based on the presence of a number in a string.
+    0 if the target number is present in the input, 1 otherwise.
+    """
+    def loss(self, inputs: Iterable[str], targets: Iterable[str]) -> np.array:
+        losses = []
+        for i, t in zip(inputs, targets):
+            pattern = fr'(^|\s|=|#)0*{t}($|\s|=|#)'
+            losses.append(1 if re.search(pattern, i) is None else 0)
+        return np.array(losses).astype("float32")
--- tests/test_dln_losses.py
+++ tests/test_dln_losses.py
@@ -1,5 +1,12 @@
 import numpy as np
-from dln.loss import ZeroOneLoss
+import pytest
+from dln.loss import LLoss, NumberPresenceLoss, ZeroOneLoss
+
+def test_instantiate_loss_from_str():
+    loss = LLoss.instantiate("ZeroOneLoss")
+    assert isinstance(loss, ZeroOneLoss)
+    with pytest.raises(ValueError, match="Unknown loss type: UnknownLoss"):
+        LLoss.instantiate("UnknownLoss")
 
 
 def test_zero_one_loss():
@@ -33,3 +40,37 @@ def test_zero_one_loss_postproc_property():
 
     zero_one_loss = ZeroOneLoss()
     assert zero_one_loss.postproc("abc") == "abc"
+
+
+def test_number_presence_loss():
+    number_presence_loss = NumberPresenceLoss()
+    inputs = [
+        "1234",
+        "01234",
+        "Answer 1234 in it.",
+        "Answer\n1234",
+        "Answer 01234 in it.",
+        "12 34",
+        "12340",
+        "Answer 12340 not in it.",
+        "Answer test_1234 not in it.",
+        "Answer 101234 not in it."
+    ]
+    targets = [1234] * 10
+    loss = number_presence_loss(inputs, targets)
+    np.testing.assert_array_equal(
+        loss, [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+    )
+
+
+def test_number_presence_loss_single_value():
+    number_presence_loss = NumberPresenceLoss()
+    assert not number_presence_loss("1234", 1234)
+    assert not number_presence_loss("01234", "1234")
+    assert not number_presence_loss("Answer 1234 in it.", "1234")
+    assert not number_presence_loss("Answer\n1234", 1234)
+    assert not number_presence_loss("Answer 01234 in it.", 1234)
+    assert number_presence_loss("12340", "1234")
+    assert number_presence_loss("Answer 12340 not in it.", 1234)
+    assert number_presence_loss("Answer test_1234 not in it.", "1234")
+    assert number_presence_loss("Answer 101234 not in it.", 1234)
