0e2ad8f43a3636c1f4d5d11dceb95ddbef0d64d5
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2024-01-12 15:27:31 -0500

    Adding scripts for phi-2

---
+++ projects/vi_dln/scripts/one_layer/phi2_Nshot.sh
@@ -0,0 +1,95 @@
+set -x  # print commands to terminal
+
+fwd_model_type="/vllm/phi-2"
+bwd_model_type="/vllm/phi-2"
+dataset="gsm8k"
+loss_function="number_presence_loss"
+output_scoring_function="accuracy"
+max_train_size=300
+max_dev_size=300
+max_test_size=300
+p_class_tpl="classify_forward:3.0"
+iters=10
+batch_size=10
+num_p_samples=10
+num_h_samples=5
+bwd_temp=0.7
+forward_use_classes=False
+held_out_prompt_ranking=True
+use_memory=2
+tolerance=2
+trust_factor=1.
+q_prompt_tpl="q_action_prompt"
+logp_penalty=1.
+posterior_temp=1.
+p_hidden_tpl="suffix_forward_tbs"
+q_hidden_tpl="suffix_forward_tbs_y|suffix_forward_tbs"
+fwd_max_tokens=256
+bwd_max_tokens=512
+one_layer=True
+
+# iters=20
+# num_p_samples=15
+
+# dataset
+for dataset in gsm8k hyperbaton navigate date_understanding logical_deduction_seven_objects mpqa trec subj disaster airline; do
+# n_shot
+for n_shot in 0 5; do
+
+# If dataset is gsm8k, then set forward_use_classes to False
+if [ ${dataset} == "gsm8k" ]; then
+    forward_use_classes=False
+else
+    forward_use_classes=True
+fi
+
+dir=log/phi2/${dataset}/${n_shot}shot/
+
+if [ ! -f ${dir}/done.txt ]; then
+    /bin/rm -rf ${dir}
+
+    # seed
+    for seed in 13 42 25; do
+        python vi_main.py \
+            --one_layer ${one_layer} \
+            --do_first_eval \
+            --do_zero_shot \
+            --n_shots ${n_shot} \
+            --fwd_model_type ${fwd_model_type} \
+            --bwd_model_type ${bwd_model_type} \
+            --dataset ${dataset} \
+            --loss_function ${loss_function} \
+            --max_train_size ${max_train_size} \
+            --max_dev_size ${max_dev_size} \
+            --max_test_size ${max_test_size} \
+            --p_class ${p_class_tpl} \
+            --iters ${iters} \
+            --batch_size ${batch_size} \
+            --num_p_samples ${num_p_samples} \
+            --num_h_samples ${num_h_samples} \
+            --bwd_temp ${bwd_temp} \
+            --forward_use_classes ${forward_use_classes} \
+            --held_out_prompt_ranking ${held_out_prompt_ranking} \
+            --out_dir ${dir} \
+            --use_memory ${use_memory} \
+            --tolerance ${tolerance} \
+            --trust_factor ${trust_factor} \
+            --q_prompt ${q_prompt_tpl} \
+            --logp_penalty ${logp_penalty} \
+            --posterior_temp ${posterior_temp} \
+            --p_hidden ${p_hidden_tpl} \
+            --q_hidden ${q_hidden_tpl} \
+            --seed ${seed} \
+            --fwd_max_tokens ${fwd_max_tokens} \
+            --bwd_max_tokens ${bwd_max_tokens} \
+            --output_scoring_function ${output_scoring_function}
+
+    # seed
+    done
+
+    touch ${dir}/done.txt
+fi
+# n shot
+done
+# dataset
+done
---
+++ projects/vi_dln/scripts/two_layers_e2e/phi2_DLN-2.sh
@@ -0,0 +1,61 @@
+set -x  # print commands to terminal
+dataset=navigate
+p_class_tpl="classify_forward:3.0"
+iters=20
+batch_size=10
+num_p_samples=10
+bwd_temp=0.7
+held_out_prompt_ranking=True
+use_memory=5
+tolerance=2
+num_h_samples=10
+q_prompt_tpl="q_action_prompt:v3.5"
+logp_penalty=2.
+posterior_temp=1.
+trust_factor=5.
+p_hidden_tpl="suffix_forward_tbs"
+q_hidden_tpl="suffix_forward_tbs_y|suffix_forward_tbs"
+fwd_model_type="/vllm/phi-2"
+bwd_model_type="gpt-35-turbo"
+
+# dataset
+for dataset in hyperbaton navigate date_understanding logical_deduction_seven_objects mpqa trec subj disaster airline; do
+
+dir=log/phi2/${dataset}/DLN-2/
+
+if [ ! -f ${dir}/done.txt ]; then
+    /bin/rm -rf ${dir}
+
+    # seed
+    for seed in 13 42 25; do
+        python vi_main.py \
+            --balance_batch \
+            --do_first_eval \
+            --num_p_samples ${num_p_samples} \
+            --num_h_samples ${num_h_samples} \
+            --bwd_temp ${bwd_temp} \
+            --iters ${iters} \
+            --p_hidden ${p_hidden_tpl} \
+            --q_hidden ${q_hidden_tpl} \
+            --q_prompt ${q_prompt_tpl} \
+            --p_class ${p_class_tpl} \
+            --out_dir ${dir} \
+            --batch_size ${batch_size} \
+            --seed ${seed} \
+            --dataset ${dataset} \
+            --use_memory ${use_memory} \
+            --tolerance ${tolerance} \
+            --held_out_prompt_ranking ${held_out_prompt_ranking} \
+            --trust_factor ${trust_factor} \
+            --train_p1 True \
+            --train_p2 True \
+            --logp_penalty ${logp_penalty} \
+            --posterior_temp ${posterior_temp} \
+            --output_scoring_function logprobs \
+            --fwd_model_type ${fwd_model_type} \
+            --bwd_model_type ${bwd_model_type}
+    done
+fi
+
+# dataset
+done
