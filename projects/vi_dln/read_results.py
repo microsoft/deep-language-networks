91148628fab5d503bc49f1dd119d6c17b80181c7
Matheus Pereira <matpereira@microsoft.com>
2024-01-12 06:26:47 -0800

    fix codeql warning?

--- projects/vi_dln/read_results.py
+++ projects/vi_dln/read_results.py
@@ -11,7 +11,7 @@ import scipy.stats
 
 
 def escape_ansi(line):
-    ansi_escape = re.compile(r"(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]")
+    ansi_escape = re.compile(r"(?:\x1B[@-_]|[\x80-\x9F])[\[0-9:;<=>?\]]*[ -/]*[@-~]")
     return ansi_escape.sub("", line)
 
 
