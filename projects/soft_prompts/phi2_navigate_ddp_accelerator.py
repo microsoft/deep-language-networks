3ad6715dc850b99509a8271a813b0503611c87fc
Matheus Pereira <matpereira@microsoft.com>
2024-02-12 08:26:51 -0800

    add comments and move preprocess_function outside main

--- projects/soft_prompts/phi2_navigate_ddp_accelerator.py
+++ projects/soft_prompts/phi2_navigate_ddp_accelerator.py
@@ -65,10 +65,51 @@ def test(dataloader, model, tokenizer, device):
     return loss
 
 
+def preprocess_function(examples, tokenizer, text_column, label_column, max_length):
+    batch_size = len(examples[text_column])
+    inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
+    targets = [str(x) for x in examples[label_column]]
+    model_inputs = tokenizer(inputs)
+    labels = tokenizer(targets)
+    for i in range(batch_size):
+        # concat the inputs and labels, mask the inputs part, and update the
+        # attention mask to match the new length (inputs + labels + pad_token_id)
+        sample_input_ids = model_inputs["input_ids"][i]
+        label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
+        # print(i, sample_input_ids, label_input_ids)
+        model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+        # masks / ignores -100 tokens in the loss: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html#crossentropyloss
+        labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+        model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+    # print(model_inputs)
+    for i in range(batch_size):
+        # pad or truncate the batch to the specified max_length, and update the attention mask
+        sample_input_ids = model_inputs["input_ids"][i]
+        label_input_ids = labels["input_ids"][i]
+        model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
+            max_length - len(sample_input_ids)
+        ) + sample_input_ids
+        model_inputs["attention_mask"][i] = [0] * (
+            max_length - len(sample_input_ids)
+        ) + model_inputs["attention_mask"][i]
+        labels["input_ids"][i] = [-100] * (
+            max_length - len(sample_input_ids)
+        ) + label_input_ids
+        model_inputs["input_ids"][i] = torch.tensor(
+            model_inputs["input_ids"][i][:max_length]
+        )
+        model_inputs["attention_mask"][i] = torch.tensor(
+            model_inputs["attention_mask"][i][:max_length]
+        )
+        labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+
 def main():
     accelerator = Accelerator()
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
+
     model_name_or_path = "microsoft/phi-2"
     tokenizer_name_or_path = "microsoft/phi-2"
 
@@ -105,42 +146,6 @@ def main():
     )
     print(target_max_length)
 
-    def preprocess_function(examples):
-        batch_size = len(examples[text_column])
-        inputs = [f"{text_column} : {x} Label : " for x in examples[text_column]]
-        targets = [str(x) for x in examples[label_column]]
-        model_inputs = tokenizer(inputs)
-        labels = tokenizer(targets)
-        for i in range(batch_size):
-            sample_input_ids = model_inputs["input_ids"][i]
-            label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
-            # print(i, sample_input_ids, label_input_ids)
-            model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
-            labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
-            model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
-        # print(model_inputs)
-        for i in range(batch_size):
-            sample_input_ids = model_inputs["input_ids"][i]
-            label_input_ids = labels["input_ids"][i]
-            model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-                max_length - len(sample_input_ids)
-            ) + sample_input_ids
-            model_inputs["attention_mask"][i] = [0] * (
-                max_length - len(sample_input_ids)
-            ) + model_inputs["attention_mask"][i]
-            labels["input_ids"][i] = [-100] * (
-                max_length - len(sample_input_ids)
-            ) + label_input_ids
-            model_inputs["input_ids"][i] = torch.tensor(
-                model_inputs["input_ids"][i][:max_length]
-            )
-            model_inputs["attention_mask"][i] = torch.tensor(
-                model_inputs["attention_mask"][i][:max_length]
-            )
-            labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
-        model_inputs["labels"] = labels["input_ids"]
-        return model_inputs
-
     processed_datasets = dataset.map(
         preprocess_function,
         batched=True,
@@ -148,6 +153,12 @@ def main():
         remove_columns=dataset["train"].column_names,
         load_from_cache_file=False,
         desc="Running tokenizer on dataset",
+        fn_kwargs={
+            "tokenizer": tokenizer,
+            "text_column": text_column,
+            "label_column": label_column,
+            "max_length": max_length,
+        },
     )
 
     train_dataset = processed_datasets["train"]
