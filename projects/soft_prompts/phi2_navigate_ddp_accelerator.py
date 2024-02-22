99330b7f071d9d986a87153037f57514ea603604
Chinmay Singh <chinmay.singh@gmail.com>
2024-02-22 10:25:18 +0000

    updated script

--- projects/soft_prompts/phi2_navigate_ddp_accelerator.py
+++ projects/soft_prompts/phi2_navigate_ddp_accelerator.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from accelerate import Accelerator
 from datasets import Dataset, DatasetDict
+from torch.utils.data import Subset
 from dln.dataset import init_dataset
 from peft import (
     PromptTuningConfig,
@@ -20,7 +21,8 @@ from transformers import (
     default_data_collator,
     get_linear_schedule_with_warmup,
 )
-
+from accelerate import Accelerator
+accelerator = Accelerator()
 
 def load_dln_dataset_to_hf_dataset(dataset_id):
     """Some gynmastics to load the dln dataset into a HuggingFace Dataset.
@@ -48,7 +50,6 @@ def load_dln_dataset_to_hf_dataset(dataset_id):
     )
     return dataset_dict
 
-
 def exact_match_loss(generated_texts, target_texts):
     losses = []
     for generated_text, target_text in zip(generated_texts, target_texts):
@@ -56,13 +57,9 @@ def exact_match_loss(generated_texts, target_texts):
         target_tokens = target_text.split()
         loss = sum(generated_token != target_token for generated_token, target_token in zip(generated_tokens, target_tokens))
         losses.append(loss)
-    
+
     loss_tensor = torch.tensor(losses, dtype=torch.float32)
     total_loss = torch.mean(loss_tensor)
-    
-    print(generated_texts)
-    print(target_texts)
-    print(total_loss.item())
     
     return total_loss
 
@@ -80,23 +77,7 @@ def test(dataloader, model, tokenizer, device, exact_match=False):
 
         loss = exact_match_loss(generated_texts, target_texts_decoded) if exact_match else outputs.loss
         loss += loss.detach().float()
-        # preds.extend(
-        #     tokenizer.batch_decode(
-        #         torch.argmax(outputs.logits, -1).detach().cpu().numpy(),
-        #         skip_special_tokens=True,
-        #     )
-        # )
         labels = torch.where(batch['labels'] != -100, batch['labels'], tokenizer.pad_token_id)
-
-        # targets = []
-        # for label_row in labels:
-        #     decoded_tokens = tokenizer.convert_ids_to_tokens(label_row, skip_special_tokens=True)
-        #     decoded_text = tokenizer.convert_tokens_to_string(decoded_tokens)
-        #     targets.append(decoded_text)
-
-        # if (exact_match):
-        #     print(preds)
-            # print(targets)
 
     loss = loss / len(dataloader)
     return loss
@@ -104,22 +85,18 @@ def test(dataloader, model, tokenizer, device, exact_match=False):
 
 def preprocess_function(examples, tokenizer, prefix, text_column, label_column, max_length):
     batch_size = len(examples[text_column])
-    inputs = [f"{prefix}\n\n{x}\n\nAnswer:\n" for x in examples[text_column]]
+    inputs = [f"{prefix}{x}\n\nAnswer:\n" for x in examples[text_column]]
     targets = [str(x) for x in examples[label_column]]
     model_inputs = tokenizer(inputs)
     labels = tokenizer(targets)
     for i in range(batch_size):
-        # concat the inputs and labels, mask the inputs part, and update the
-        # attention mask to match the new length (inputs + labels + pad_token_id)
+        # Mask the inputs part, and update the attention mask to match the new length
         sample_input_ids = model_inputs["input_ids"][i]
         label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
-        # print(i, sample_input_ids, label_input_ids)
-        model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
-        # masks / ignores -100 tokens in the loss: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html#crossentropyloss
+        model_inputs["input_ids"][i] = sample_input_ids
+        # masks / ignores -100 tokens in the loss
         labels["input_ids"][i] = [tokenizer.pad_token_id] * len(sample_input_ids) + label_input_ids
         model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
-    # print(model_inputs)
-    for i in range(batch_size):
         # pad or truncate the batch to the specified max_length, and update the attention mask
         sample_input_ids = model_inputs["input_ids"][i]
         label_input_ids = labels["input_ids"][i]
@@ -130,7 +107,7 @@ def preprocess_function(examples, tokenizer, prefix, text_column, label_column, 
             max_length - len(sample_input_ids)
         ) + model_inputs["attention_mask"][i]
         labels["input_ids"][i] = [tokenizer.pad_token_id] * (
-            max_length - len(sample_input_ids)
+            max_length - len(label_input_ids)
         ) + label_input_ids
         model_inputs["input_ids"][i] = torch.tensor(
             model_inputs["input_ids"][i][:max_length]
@@ -144,7 +121,6 @@ def preprocess_function(examples, tokenizer, prefix, text_column, label_column, 
 
 
 def main():
-    accelerator = Accelerator()
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     model_name_or_path = "microsoft/phi-2"
@@ -152,15 +128,14 @@ def main():
 
     dataset_id = "navigate"
     initial_instruction = (
-        "Read the following sentence, then determine whether you return to the starting point."
+        "Read the following question, then choose the correct answer."
     )
     text_column = "text"
     label_column = "label"
     max_length = 128
     lr = 3e-2
-    num_epochs = 10
-    # batch_size = 8
-    batch_size = 16
+    num_epochs = 3
+    batch_size = 10
 
     peft_config = PromptTuningConfig(
         task_type=TaskType.CAUSAL_LM,
@@ -174,7 +149,7 @@ def main():
 
     classes = list(set(dataset["train"]["label"]))
 
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, device_map="auto", padding_side='left')
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token_id = tokenizer.eos_token_id
     target_max_length = max(
@@ -191,17 +166,19 @@ def main():
         desc="Running tokenizer on dataset",
         fn_kwargs={
             "tokenizer": tokenizer,
-            "prefix": initial_instruction,
+            "prefix": initial_instruction + "\n\n",
             "text_column": text_column,
             "label_column": label_column,
             "max_length": max_length,
         },
     )
+    
+    indices = list(range(20))
 
-    train_dataset = processed_datasets["train"]
-    eval_dataset = processed_datasets["dev"]
-    test_dataset = processed_datasets["test"]
-
+    train_dataset = Subset(processed_datasets["train"], indices)
+    eval_dataset = Subset(processed_datasets["dev"], indices)
+    test_dataset = Subset(processed_datasets["test"], indices)
+    
     train_dataloader = DataLoader(
         train_dataset,
         shuffle=True,
@@ -224,7 +201,6 @@ def main():
 
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
     model = get_peft_model(model, peft_config)
-    print(model.print_trainable_parameters())
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     lr_scheduler = get_linear_schedule_with_warmup(
@@ -250,23 +226,19 @@ def main():
         total_loss = 0
         for step, batch in enumerate(tqdm(train_dataloader)):
             batch = {k: v.to(device) for k, v in batch.items()}
+            output = model.generate(**batch, max_length=1, num_return_sequences=1, return_dict_in_generate=True)
 
-            output = model.generate(batch["input_ids"], max_length=500, num_return_sequences=1)
-
-            generated_texts = [tokenizer.decode(out, skip_special_tokens=True) for out in output]    
+            generated_texts = [tokenizer.decode(out[-1], skip_special_tokens=True) for out in output.sequences] #tokenizer.batch_decode(output.sequences, skip_special_tokens=True, clean_up_tokenization_spaces=False)
             target_texts_decoded = [tokenizer.decode(target, skip_special_tokens=True) for target in batch["labels"]]
 
             loss = exact_match_loss(generated_texts, target_texts_decoded)
-            # optimizer.zero_grad()
             loss.requires_grad_(True)
-            
+
             total_loss += loss.detach().float()
             optimizer.zero_grad()
-
             accelerator.backward(loss)
             optimizer.step()
             lr_scheduler.step()
-            # optimizer.zero_grad()
 
         model.eval()
         eval_epoch_loss = test(eval_dataloader, model, tokenizer, device)
@@ -282,28 +254,6 @@ def main():
     final_test_ppl = torch.exp(final_test_loss)
     print(f"Test before training: {init_test_ppl=} {init_test_loss=}")
     print(f"Test after training: {final_test_ppl=} {final_test_loss=}")
-
-    model.module.save_pretrained("data/models/" + model_name_or_path)
-
-    config = PeftConfig.from_pretrained("data/models/" + model_name_or_path)
-    model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
-
-    lora_model = PeftModel.from_pretrained(model, "data/models/" + model_name_or_path)
-    lora_model.to(device)
-
-    # final_test_loss = test(test_dataloader, lora_model, tokenizer, device, True)
-    # final_test_ppl = torch.exp(final_test_loss)
-
-    # print(f"Test after loading: {final_test_ppl=} {final_test_loss=}")
-
-    sentences = ["Read the following sentence, then determine whether you return to the starting point.\n\nIf you follow these instructions, do you return to the starting point? Take 9 steps. Take 9 steps. Take 4 steps. Turn right.\nOptions:\n- Yes\n- No\n\nAnswer:\n"]
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-    tokenizer.padding_side = "left"
-
-    inputs = tokenizer(sentences, return_tensors="pt", padding=True).to(device)
-    generate_ids = lora_model.generate(**inputs, max_length=500)
-    outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-    print([print(o, "\n") for o in outputs])
 
 if __name__ == "__main__":
     main()
