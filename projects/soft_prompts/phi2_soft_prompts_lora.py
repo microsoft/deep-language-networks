9c37fa1b61aed0d4d951e570abcf1ef4d8d7ae68
Chinmay Singh <chsingh@microsoft.com>
2024-04-17 04:57:56 +0000

    remove notebooks

--- projects/soft_prompts/phi2_soft_prompts_lora.py
+++ projects/soft_prompts/phi2_soft_prompts_lora.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# ## Multitask prompt tuning using Phi-2
+# ## LoRA using Phi-2
 
 # %%
 import os
@@ -14,8 +14,36 @@ model_id = "microsoft/phi-2"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side="left")
 
+from accelerate import Accelerator
+accelerator = Accelerator()
+
 tokenizer.pad_token_id = tokenizer.eos_token_id
 tokenizer.padding_side = "left"
+
+# %%
+from peft import PeftModel
+
+model = None
+saved_model = None
+
+try:
+    sentences = ["Read the following sentence, then determine whether you return to the starting point.\n\nIf you follow these instructions, do you return to the starting point? Take 9 steps. Take 9 steps. Take 4 steps. Turn right.\nOptions:\n- Yes\n- No\n\nAnswer:\n"]
+    inputs = tokenizer(sentences, return_tensors="pt", padding=True).to(device)
+
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    model.to(device)
+    generate_ids = model.generate(**inputs, max_length=500)
+    outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+    print(outputs[0])
+
+    print("Using saved model from data/models/" + model_id)
+    saved_model = PeftModel.from_pretrained(model, "data/models/" + model_id + "/lora")
+    saved_model.to(device)
+    generate_ids = saved_model.generate(**inputs, max_length=500)
+    outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+    print(outputs[0])
+except ValueError:
+    print("Model not found, training new model")
 
 # %%
 def preprocess_function(examples, tokenizer, prefix, text_column, label_column, max_length):
@@ -31,11 +59,7 @@ def preprocess_function(examples, tokenizer, prefix, text_column, label_column, 
     # Replace padding tokens in the labels with -100
     labels["input_ids"] = [[-100 if token == tokenizer.pad_token_id else token for token in label] for label in labels["input_ids"]]
 
-    task_ids = [0 for i in labels["input_ids"]]
-    task_ids = torch.tensor(task_ids)
-
     model_inputs["labels"] = labels["input_ids"]
-    model_inputs["task_ids"] = task_ids
     return model_inputs
 
 # %%
@@ -61,26 +85,21 @@ def exact_match_loss(outputs, labels):
         loss = sum(generated_token != target_token for generated_token, target_token in zip(generated_tokens, target_tokens))
         losses.append(loss)
 
-    loss_tensor = torch.tensor(losses, dtype=torch.float32).to(device)
+    loss_tensor = torch.tensor(losses, dtype=torch.float32)
     total_loss = torch.mean(loss_tensor)
     return total_loss, generated_texts
 
 # %%
-def test(dataloader, model, exact_match=True):
+def test(dataloader, model, tokenizer, device, exact_match=True):
     total_loss = 0
     test_preds = []
     for batch in tqdm(dataloader):
         batch = {k: v.to(device) for k, v in batch.items()}
         with torch.no_grad():
-            output1 = model(**batch, output_hidden_states=True)
-            loss, preds = exact_match_loss(output1, batch["labels"]) if exact_match else (output1.loss, test_preds)
-            sequence_length_labels = batch['labels'].shape[1]
-            inputs_embeds = output1.hidden_states[-1][:, -sequence_length_labels:]
-            task_ids = torch.tensor([1 for i in batch["task_ids"]]).to(device)
-            output2 = model(inputs_embeds=inputs_embeds, labels=batch['labels'], attention_mask=batch['attention_mask'], task_ids=task_ids, output_hidden_states=True)
-            loss, preds = exact_match_loss(output2, batch["labels"]) if exact_match else (output2.loss, test_preds)
-    
+            outputs = model(**batch)
+        loss, preds = exact_match_loss(outputs, batch["labels"]) if exact_match else (outputs.loss, [])
         total_loss += loss.detach().float()
+        labels = torch.where(batch['labels'] != -100, batch['labels'], tokenizer.pad_token_id)
         test_preds.extend(preds)
 
     total_loss = total_loss / len(dataloader)
@@ -98,7 +117,7 @@ def load_dln_dataset_to_hf_dataset(dataset_id):
     dln_dataset = init_dataset(
         dataset_id=dataset_id,
         seed=42,
-        data_dir="../../data",
+        data_dir=os.path.dirname(os.getcwd()) + "/../data",
     )
 
     def load_split(split):
@@ -118,21 +137,10 @@ def load_dln_dataset_to_hf_dataset(dataset_id):
     return dataset_dict
 
 # %%
-from accelerate import Accelerator
 accelerator = Accelerator()
-
-import random
-def set_seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
 from peft import (
-    MultitaskPromptTuningConfig,
-    MultitaskPromptTuningInit,
+    LoraConfig,
     TaskType,
-    PeftModel,
     get_peft_model,
 )
 
@@ -148,9 +156,16 @@ import torch.nn as nn
 import torch.distributed as dist
 from torch.utils.data import Subset
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+import random
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 set_seed(42)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model_name_or_path = "microsoft/phi-2"
 tokenizer_name_or_path = "microsoft/phi-2"
@@ -162,18 +177,12 @@ initial_instruction = (
 text_column = "text"
 label_column = "label"
 max_length = 128
-lr = 3e-2
-num_epochs = 50
+lr = 1e-4
+num_epochs = 10
 batch_size = 8
 
-peft_config = MultitaskPromptTuningConfig(
-    task_type=TaskType.CAUSAL_LM,
-    num_tasks=2,
-    prompt_tuning_init=MultitaskPromptTuningInit.TEXT,
-    num_virtual_tokens=8,
-    prompt_tuning_init_text=initial_instruction,
-    num_transformer_submodules=1,
-    tokenizer_name_or_path=model_name_or_path,
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM
 )
 
 dataset = load_dln_dataset_to_hf_dataset(dataset_id)
@@ -183,6 +192,10 @@ classes = list(set(dataset["train"]["label"]))
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, device_map="auto", padding_side='left')
 if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
+target_max_length = max(
+    [len(tokenizer(class_label)["input_ids"]) for class_label in classes]
+)
+print(target_max_length)
 
 processed_datasets = dataset.map(
     preprocess_function,
@@ -224,24 +237,21 @@ test_dataloader = DataLoader(
     pin_memory=True,
 )
 
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
-model.config.pad_token_id = model.config.eos_token_id
-
-saved_model = None
-try:
-    saved_model = PeftModel.from_pretrained(model, "data/models/" + model_name_or_path + "/multitask")
-    model = saved_model
-    print("Using saved model from data/models/" + model_name_or_path)
-except ValueError:
+if saved_model is None:
+    if model is None:
+        model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+    model.config.pad_token_id = model.config.eos_token_id
     model = get_peft_model(model, peft_config)
-    print("Model not found, training new model")
-
+else:
+    model = saved_model
+    model.enable_input_require_grads()
+    print("Using saved model from data/models/" + model_name_or_path)
+    
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-lr_scheduler= get_linear_schedule_with_warmup(
+lr_scheduler = get_linear_schedule_with_warmup(
     optimizer=optimizer,
     num_warmup_steps=0,
     num_training_steps=(len(train_dataloader) * num_epochs),
-
 )
 
 model = model.to(device)
@@ -252,28 +262,18 @@ train_dataloader, eval_dataloader, test_dataloader, model, optimizer = accelerat
 )
 
 model.eval()
-
-init_test_loss, test_preds = test(test_dataloader, model)
+init_test_loss, test_preds = test(test_dataloader, model, tokenizer, device)
 init_test_ppl = torch.exp(init_test_loss)  # Perplexity
 print(f"Test before training: {init_test_ppl=} {init_test_loss=}")
-
-best_eval_loss = float('inf')
-epochs_without_improvement = 0
-patience = 10  # Number of epochs to wait before stopping
 
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
     for step, batch in enumerate(tqdm(train_dataloader)):
         batch = {k: v.to(device) for k, v in batch.items()}
-        output1 = model(**batch, output_hidden_states=True)
-        
-        sequence_length_labels = batch['labels'].shape[1]
-        inputs_embeds = output1.hidden_states[-1][:, -sequence_length_labels:]
-        task_ids = torch.tensor([1 for i in batch["task_ids"]]).to(device)
-        output2 = model(inputs_embeds=inputs_embeds, labels=batch['labels'], attention_mask=batch['attention_mask'], task_ids=task_ids, output_hidden_states=True)
-       
-        loss = output1.loss + output2.loss
+        output = model(**batch)
+
+        loss = output.loss
         total_loss += loss.item()
         optimizer.zero_grad()
         accelerator.backward(loss)
@@ -281,84 +281,38 @@ for epoch in range(num_epochs):
         lr_scheduler.step()
 
     model.eval()
-    eval_epoch_loss, eval_preds = test(eval_dataloader, model, False)
+    eval_epoch_loss, eval_preds = test(eval_dataloader, model, tokenizer, device, False)
     eval_ppl = torch.exp(eval_epoch_loss)
     train_epoch_loss = total_loss / len(train_dataloader)
     train_ppl = torch.exp(torch.tensor(train_epoch_loss))
-
     print(
         f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}"
     )
-    
-    # Sum the eval losses from all processes
-    if dist.is_available() and dist.is_initialized() and dist.get_rank() == 0:
-        # In the main process, prepare to receive the sum of eval losses
-        total_eval_loss = torch.zeros_like(eval_epoch_loss)
-    else:
-        total_eval_loss = eval_epoch_loss
-
-    if dist.is_available() and dist.is_initialized():
-        dist.reduce(total_eval_loss, dst=0, op=dist.ReduceOp.SUM)
-        # Broadcast the total eval loss from the main process to all other processes
-        dist.broadcast(total_eval_loss, src=0)
-        eval_epoch_loss = total_eval_loss / dist.get_world_size()
-
-    # Check if the validation loss has improved
-    if eval_epoch_loss < best_eval_loss:
-        best_eval_loss = eval_epoch_loss
-        epochs_without_improvement = 0
-    else:
-        epochs_without_improvement += 1
-
-    # if epochs_without_improvement >= patience:
-    #     print("Stopping training: ", dist.get_rank())
-    #     break
 
 model.eval()
-
-final_test_loss, test_preds = test(test_dataloader, model)
-final_test_ppl = torch.exp(final_test_loss)
-print(f"Test before training1: {init_test_ppl=} {init_test_loss=}")
-print(f"Test after training1: {final_test_ppl=} {final_test_loss=}")
-
 if not saved_model:
-    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
-        model.module.save_pretrained("data/models/" + model_name_or_path + "/multitask")
-    else:
-        model.save_pretrained("data/models/" + model_name_or_path + "/multitask")
+    model.save_pretrained("data/models/" + model_name_or_path + "/lora")
+
+final_test_loss, test_preds = test(test_dataloader, model, tokenizer, device)
+final_test_ppl = torch.exp(final_test_loss)
+print(f"Test before training: {init_test_ppl=} {init_test_loss=}")
+print(f"Test after training: {final_test_ppl=} {final_test_loss=}")
 
 # %%
-# Ensure final_test_loss is on the correct device
-final_test_loss = final_test_loss.cuda()
+correct = 0
+total = 0
+for pred, label in zip(test_preds,  dataset['test']['label']):
+    if pred.strip() == label.strip():
+        correct += 1
+    total += 1
+accuracy = correct / total * 100
 
-# Use dist.reduce() to add final_test_loss from all GPUs
-if dist.is_available() and dist.is_initialized():
-    # The reduce operation sums all the final_test_loss and stores the result in rank 0
-    dist.reduce(final_test_loss, dst=0, op=dist.ReduceOp.SUM)
+print(f"{accuracy=}% on the test dataset")
+print(f"{test_preds[:10]=}")
+print(f"{dataset['test']['label'][:10]=}")
 
-if dist.is_available() and dist.is_initialized():
-    dist.barrier()
-
-# Gather test_preds from all GPUs to the main GPU
-if dist.is_available() and dist.is_initialized():
-    if dist.get_rank() == 0:
-        accuracy = round(100 * (1- (final_test_loss.item() / dist.get_world_size())), 1)
-        print(f"{test_preds[:10]=}")
-        print(f"{dataset['test']['label'][:10]=}")
-        print(f"{accuracy=}% on the test dataset")
-else:
-    correct = 0
-    total = 0
-    for pred, label in zip(test_preds, dataset['test']['label']):
-        if pred.strip() == label.strip():
-            correct += 1
-        total += 1
-    accuracy = correct / total * 100
-
-    print(f"{test_preds[:10]=}")
-    print(f"{dataset['test']['label'][:10]=}")
-    print(f"{accuracy=}% on the test dataset")
-
+"accuracy=84.8% on the test dataset"
 "test_preds[:10]=['Yes', 'No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes']"
 "dataset['test']['label'][:10]=['No', 'No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'No', 'No']"
-"accuracy=73.4% on the test dataset"
+
+
