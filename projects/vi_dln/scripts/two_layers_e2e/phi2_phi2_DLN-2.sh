7ea71ee91be10d4ddcd5b8b8c4573ec56ee2813f
Chinmay Singh <chinmay.singh@gmail.com>
2024-04-21 00:52:36 +0000

    reduce batch size

--- projects/vi_dln/scripts/two_layers_e2e/phi2_phi2_DLN-2.sh
+++ projects/vi_dln/scripts/two_layers_e2e/phi2_phi2_DLN-2.sh
@@ -2,7 +2,7 @@ set -x  # print commands to terminal
 dataset=navigate
 p_class_tpl="classify_forward:3.0"
 iters=20
-batch_size=10
+batch_size=1
 num_p_samples=10
 bwd_temp=0.7
 held_out_prompt_ranking=True
